5cef70d31c37a2ea7fd0c12e53519ad38ff8d0ff
Thomas De Meyer <thomas.demeyer@incentro.com>
2022-08-24 17:41:04 +0200

    main: added full example

---
+++ examples/full/main.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    commercelayer = {
+      version = ">= 0.0.1"
+      source  = "incentro-dc/commercelayer"
+    }
+  }
+}
+
+provider "commercelayer" {
+
+}
