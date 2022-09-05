0b83a345a9ac610e67f881c854112bd116717a11
tolga-akyazi <tolga.akyazi@incentro.com>
2022-09-05 14:02:17 +0200

    changed description of customer groups resource

--- commercelayer/resource_customer_groups.go
+++ commercelayer/resource_customer_groups.go
@@ -9,8 +9,10 @@ import (
 
 func resourceCustomerGroup() *schema.Resource {
 	return &schema.Resource{
-		Description: "A CustomerGroup is the fiscal representative that is selling in a specific market. Tax calculators " +
-			"use the CustomerGroup's address (and the shipping address) to determine the tax rate for an order.",
+		Description: `A customer group is a resource that can be used to organize customers into groups. 
+		When you associate a customer group to a market, that market becomes private and can be accessed
+		 only by the customers belonging to the group. You can use customer groups to manage B2B customers, 
+		 B2C loyalty programs, private sales, and more.`,
 		ReadContext:   resourceCustomerGroupReadFunc,
 		CreateContext: resourceCustomerGroupCreateFunc,
 		UpdateContext: resourceCustomerGroupUpdateFunc,
