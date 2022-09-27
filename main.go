b5058148e14c6381f9776b8a090837ad2d80d954
Thomas De Meyer <thomas.demeyer@incentro.com>
2022-09-27 11:20:30 +0200

    feature: added linting and cleaned up imports

--- main.go
+++ main.go
@@ -1,18 +1,10 @@
 package main
 
 import (
-	"context"
 	"flag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 	"github.com/incentro-dc/terraform-provider-commercelayer/commercelayer"
-	"log"
 )
-
-// Run "go generate" to format example terraform files and generate the docs for the registry/website
-
-// If you do not have terraform installed, you can remove the formatting command, but its suggested to
-// ensure the documentation is formatted properly.
-//go:generate terraform fmt -recursive ./examples/
 
 // Run the docs generation tool, check its repository for more information on how it works and how docs
 // can be customized.
@@ -26,11 +18,8 @@ func main() {
 	opts := &plugin.ServeOpts{ProviderFunc: commercelayer.Provider}
 
 	if debugMode {
-		err := plugin.Debug(context.Background(), "registry.terraform.io/incentro/commercelayer", opts)
-		if err != nil {
-			log.Fatal(err.Error())
-		}
-		return
+		opts.Debug = true
+		opts.ProviderAddr = "registry.terraform.io/incentro/commercelayer"
 	}
 
 	plugin.Serve(opts)
