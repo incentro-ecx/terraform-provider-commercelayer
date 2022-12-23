f8a0e9cf15b0381be2cc202a60b15efa1457d4fb
tolga-akyazi <tolga.akyazi@incentro.com>
2022-12-23 12:50:24 +0100

    Stripe gateway resource

--- examples/full/stripe_gateways.tf
+++ examples/full/stripe_gateways.tf
@@ -1,6 +1,7 @@
 resource "commercelayer_stripe_gateway" "incentro_stripe_gateway" {
   attributes {
     name = "Incentro Stripe Gateway"
+    login = "SecretPassword"
     metadata = {
       foo : "bar"
     }
