def313086a624046d649c841c68b8e69c0eda6ef
tolga-akyazi <tolga.akyazi@incentro.com>
2022-12-23 12:46:57 +0100

    Stripe gateway resource

--- commercelayer/resource_stripe_gateway_test.go
+++ commercelayer/resource_stripe_gateway_test.go
@@ -61,6 +61,7 @@ func testAccStripeGatewayCreate(testName string) string {
 		resource "commercelayer_stripe_gateway" "incentro_stripe_gateway" {
            attributes {
 			name                   = "Incentro Stripe Gateway"
+			login                  = "SecretPassword"
 			metadata = {
 				foo: "bar"
 				testName: "{{.testName}}"
