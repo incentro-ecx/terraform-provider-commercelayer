1c68a3bf1f33eb18a45cec1176cf16603b0fb9d7
tolga-akyazi <tolga.akyazi@incentro.com>
2023-02-01 13:25:35 +0100

    Added unit tests for payment source validation.

--- commercelayer/validation_test.go
+++ commercelayer/validation_test.go
@@ -14,3 +14,13 @@ func TestCurrencyCodeValidationOK(t *testing.T) {
 	diag := currencyCodeValidation("EUR", nil)
 	assert.False(t, diag.HasError())
 }
+
+func TestPaymentSourceValidationError(t *testing.T) {
+	diag := paymentSourceValidation("Adyen", nil)
+	assert.True(t, diag.HasError())
+}
+
+func TestPaymentSourceValidationOK(t *testing.T) {
+	diag := paymentSourceValidation("BraintreePayment", nil)
+	assert.False(t, diag.HasError())
+}
