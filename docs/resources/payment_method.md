aaa76b61d3d962db6ab3499502b27f9dd97c73ee
Thomas De Meyer <thomas.demeyer@incentro.com>
2023-05-05 14:04:13 +0200

    Feat/additional adyen fields (#94)
    * fix: added missing adyen fields
    
    * fix: added remove mocks script

--- docs/resources/payment_method.md
+++ docs/resources/payment_method.md
@@ -15,10 +15,11 @@ Payment methods represent the type of payment sources (e.g., Credit Card, PayPal
 ```terraform
 resource "commercelayer_adyen_gateway" "incentro_adyen_gateway" {
   attributes {
-    name = "Incentro Adyen Gateway"
-    metadata = {
-      foo : "bar"
-    }
+    name             = "Incentro Adyen Gateway"
+    merchant_account = "xxxx-yyyy-zzzz"
+    api_key          = "xxxx-yyyy-zzzz"
+    public_key       = "xxxx-yyyy-zzzz"
+    live_url_prefix  = "1797a841fbb37ca7-AdyenDemo"
   }
 }
 
--- examples/full/adyen_gateways.tf
+++ examples/full/adyen_gateways.tf
@@ -1,9 +1,12 @@
 resource "commercelayer_adyen_gateway" "incentro_adyen_gateway" {
   attributes {
-    name             = "Incentro Adyen Gateway"
-    merchant_account = "xxxx-yyyy-zzzz"
-    api_key          = "xxxx-yyyy-zzzz"
-    public_key       = "xxxx-yyyy-zzzz"
-    live_url_prefix  = "1797a841fbb37ca7-AdyenDemo"
+    name                    = "Incentro Adyen Gateway"
+    merchant_account        = "xxxx-yyyy-zzzz"
+    api_key                 = "xxxx-yyyy-zzzz"
+    public_key              = "xxxx-yyyy-zzzz"
+    live_url_prefix         = "1797a841fbb37ca7-AdyenDemo"
+    api_version             = 68
+    async_api               = true
+    webhook_endpoint_secret = "foobar"
   }
 }
