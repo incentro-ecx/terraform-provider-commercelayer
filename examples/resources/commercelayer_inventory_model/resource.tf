1a2666b9ab4d46f47756f2d860d3f40ffa8fa0ed
Jonne Kats <jonnekats@touchtribe.nl>
2024-10-27 15:51:39 +0100

    Update example

--- examples/resources/commercelayer_inventory_model/resource.tf
+++ examples/resources/commercelayer_inventory_model/resource.tf
@@ -1,7 +1,10 @@
 resource "commercelayer_inventory_model" "incentro_inventory_model" {
   attributes {
-    name                   = "Incentro Inventory Model"
-    stock_locations_cutoff = 2
-    strategy               = "split_shipments"
+    name                        = "Incentro Inventory Model"
+    stock_locations_cutoff      = 2
+    strategy                    = "split_shipments"
+    manual_stock_decrement      = true
+    stock_reservation_cutoff    = 4000
+    put_stock_transfers_on_hold = true
   }
 }
